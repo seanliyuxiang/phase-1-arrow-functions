469bbb0b69d4caded56d962f3594c7b9b73ce2b7
Yuxiang Li <seanliyuxiang@gmail.com>
2021-08-31 15:44:50 -0500

    commit tree 3c126e28b16897255b66bb8fe79982fe13d0b013

---
+++ index.js
@@ -0,0 +1,9 @@
+const divide = function() {
+  return 2000 / 100;
+};
+
+const square = x => Math.pow(x, 2);
+
+const add = (x, y) => {
+  return x + y;     // needs explicit return keyword if arrow-function body is more than 1 line
+};
